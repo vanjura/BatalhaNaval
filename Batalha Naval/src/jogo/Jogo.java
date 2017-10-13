878db04c1800af9720928cfad3302665dd8de59d
vanjura <lucasvanjura1996@gmail.com>
2017-10-13 14:02:06 -0300

    Organização
    - Retirados os parâmetros de teste para uma melhor organização do  código

--- Batalha Naval/src/jogo/Jogo.java
+++ Batalha Naval/src/jogo/Jogo.java
@@ -17,12 +17,14 @@ public class Jogo {
     public static void main(String[] args) {
         UIManager.put("OptionPane.messageFont", new Font("Lucida Console", Font.PLAIN, 12));
         Jogador player[] = new Jogador[4];
-        player[0] = new Jogador("P0", "Vanjura 01", "nasc");
-        player[1] = new Jogador("P1", "Penczkoski 01", "nasc");
-        player[2] = new Jogador("P2", "Vanjura 02", "nasc");
-        player[3] = new Jogador("P3", "Penczkoski 02", "nasc");
+        player[0] = new Jogador("Jogador 01"); 
+        player[1] = new Jogador("Jogador 02");
+        player[2] = new Jogador("Jogador 03");
+        player[3] = new Jogador("Jogador 04");
+        //O parametro passado com o nome do jogador serve somente 
+        //para localização do usuário durante o jogo;
         Batalha battle = new Batalha();
-        battle.executar(player);
+        battle.executar(player); //Vetor e players é passado como parâmetro
     }
 
 }
